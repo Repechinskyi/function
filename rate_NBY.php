8877d4f03be7d9ee89e95415045fae157d112779
Aleksander <80034131+Repechinskyi@users.noreply.github.com>
2021-06-22 12:13:51 +0300

    Add files via upload

---
+++ rate_NBY.php
@@ -0,0 +1,12 @@
+
+/*
+* REST API Получения курса валют в реальном времени
+*/
+
+$date = date("Ymd");
+$url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=EUR&date={$date}&json";
+$sr = file_get_contents($url);
+$sr = json_decode($sr, true);
+$euro = $sr[0]['rate'];
+
+echo $euro;
